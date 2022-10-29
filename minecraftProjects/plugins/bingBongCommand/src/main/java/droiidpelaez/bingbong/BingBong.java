223baa5b0b35aa4364788dc9dd998981f8fc1117
Jroid <joncpelaezio@gmail.com>
2022-10-29 18:09:02 -0400

    Re-organized the minecaft project file, and uploaded a new starter minecraft plugin

---
+++ minecraftProjects/plugins/bingBongCommand/src/main/java/droiidpelaez/bingbong/BingBong.java
@@ -0,0 +1,17 @@
+package droiidpelaez.bingbong;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BingBong extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic - This is where we'
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
